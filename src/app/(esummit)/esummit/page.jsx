f71b7f8cb7292175f3068ef88fc45142c275d275
SkSin19 <sakshamsinha9999@gmail.com>
2025-08-07 21:28:00 +0530

    nothing

--- src/app/(esummit)/esummit/page.jsx
+++ src/app/(esummit)/esummit/page.jsx
@@ -1,9 +1,11 @@
+import Support from "@/components/customer-support/Support";
 import Hero from "@/components/esummit/Hero-section/Hero";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <Support />
     </main>
   )
 }
